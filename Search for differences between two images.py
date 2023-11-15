d64e9bd30947be57257861721b96c74b756e65a2
Andrey Svistunov <120823381+Ersildan@users.noreply.github.com>
2023-11-15 20:48:13 +0300

    push new text in file
    new comments

--- Search for differences between two images.py
+++ Search for differences between two images.py
@@ -1,7 +1,7 @@
 from PIL import Image, ImageChops
 
 image_1 = Image.open(r'C:\ ...') # the path to the image
-image_2 = Image.open(r'C:\C:\ ...') № the path to the image
+image_2 = Image.open(r'C:\ ...') № the path to the image
 
 
 result = ImageChops.difference(image_1, image_2)
@@ -9,3 +9,5 @@ result.show()
 
 # result.getbbox() в данном случае вернет (0, 0, 888, 666)
 result.save('result.jpg')
+
+# Создаст картинку, в которой будет заметно различия между двумя изображениями
