fdff0b82395f01479c0fc0d528938144b5259747
Andrey Svistunov <120823381+Ersildan@users.noreply.github.com>
2024-01-29 12:29:59 +0300

    update text

--- creat_qrCODE.py
+++ creat_qrCODE.py
@@ -4,8 +4,7 @@ data = 'https://vk.com/ofeonttse' #push your link to create QRcode
 
 img = qrcode.make(data)
 
-№save new png on your download
+#save new png on your download
 img.save("qrcode.png")
 
-print('Hello guys')
-print('nice program')
+"Just do your Qrcode"
